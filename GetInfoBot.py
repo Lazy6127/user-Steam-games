5e2df0f1d492332091088ea05f50f6f4c67c0d39
Lazy6127 <128479934+Lazy6127@users.noreply.github.com>
2023-03-21 14:39:42 +0300

    Add files via upload

---
+++ GetInfoBot.py
@@ -0,0 +1,14 @@
+import requests
+
+class GetInfo:
+    def __init__(self):
+        self.KEY = '4D62ECBE862770EEAF64EA0E13CACBA3'
+
+    def user_games(self, steamid):
+        response = requests.get(url='http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001', params={'key':self.KEY, 'steamid':steamid, 'include_appinfo':True}).json()
+        games = ''
+        for game in response['response']['games']:
+            games += game['name'] + '\n'
+        return games
+
+# '76561199144113035'
---
+++ telebot.py
@@ -0,0 +1,20 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from GetInfoBot import GetInfo
+
+TOKEN = '1747416709:AAH31_v37EuvyEp4wDBtnt0VLngtxVYKZf8'
+bot = Bot(TOKEN)
+disp = Dispatcher(bot)
+
+#kb = ReplyKeyboardMarkup(resize_keyboard=True)
+#kb.add(KeyboardButton('Get user games'))
+
+@disp.message_handler(content_types=['text'])
+async def show_info_about_user(message: types.Message):
+    await message.answer(text=GetInfo().user_games(message.text)) #, reply_markup=kb)
+
+@disp.message_handler(commands=['start'])
+async def show_answer_to_message(message: types.Message):
+    await message.answer(text='Type steamid of any user') #, reply_markup=kb)
+
+executor.start_polling(disp)
