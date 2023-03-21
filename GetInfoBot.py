1c43fa73a89cf26c279798175de0c8a43af995e4
Lazy6127 <128479934+Lazy6127@users.noreply.github.com>
2023-03-21 14:43:26 +0300

    Update GetInfoBot.py

--- GetInfoBot.py
+++ GetInfoBot.py
@@ -2,7 +2,7 @@ import requests
 
 class GetInfo:
     def __init__(self):
-        self.KEY = '4D62ECBE862770EEAF64EA0E13CACBA3'
+        self.KEY = 'STEAM_KEY'
 
     def user_games(self, steamid):
         response = requests.get(url='http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001', params={'key':self.KEY, 'steamid':steamid, 'include_appinfo':True}).json()
